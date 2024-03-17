7fd9ff242b2c8718e8d903d131fc0ed20b025810
cbernabe1 <bernabealan05@gmail.com>
2024-03-17 20:00:25 +0800

    third commit

--- index.js
+++ index.js
@@ -5,9 +5,45 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const db = new pg.Client({
+    user: "postgres",
+    password: "localhost",
+    database: "booklibrary",
+    password: "pg123",
+    port: 5432
+});
+db.connect();
 
 app.get("/",(req,res) =>{
     res.render("loginpage.ejs");
+});
+
+app.get("/signup",(req,res)=>{
+    res.render("signuppage.ejs");
+});
+
+app.post("/register", async(req,res)=>{
+const email = req.body.email;
+const password = req.body.password;
+try {
+    const check = await db.query("SELECT email FROM users WHERE email = $1",[email]);
+    if(check.rowCount === 0){
+    const result = await db.query("INSERT INTO users (email,password) VALUES ($1,$2)",[email,password]);
+    if(result.rowCount === 1){
+        res.redirect("/");
+    }
+    else{
+        res.render("signuppage.ejs",{error: "Error inserting the account."})
+    }
+}else{
+    res.render("signuppage.ejs",{error: "Email is already existed."})
+}
+} catch (error) {
+    console.log(error);
+}
 });
 
 app.listen(port,()=>{
