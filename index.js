e3097b78dc92f449de50e3c010e308620da12f0e
cbernabe1 <bernabealan05@gmail.com>
2024-03-18 20:16:47 +0800

    fifth commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const db = new pg.Client({
 db.connect();
 const currentUser = [];
 const date = new Date().toJSON();
-let books = [];
+
 app.get("/",(req,res) =>{
     res.render("loginpage.ejs");
 });
@@ -56,11 +56,8 @@ try {
     
     currentUser.push(result.rows[0]);
     if(result.rowCount === 1){
-        const fetchbooks = await db.query("SELECT * FROM books WHERE user_id = $1",[result.rows[0].id])
-        fetchbooks.rows.forEach((book) =>{
-            books.push(book);
-        });
-        res.render("homepage.ejs",{username: email, books: books});
+        const book = await getBooks();
+        res.render("homepage.ejs",{username: email, books: book});
     }else{
         res.redirect("/");
     }
@@ -75,11 +72,12 @@ app.get("/addpage",(req,res)=>{
 
 async function getBooks(){
 try {
+    let books = [];
     const fetchbooks = await db.query("SELECT * FROM books WHERE user_id = $1",[currentUser[0].id]);
     fetchbooks.rows.forEach((book) =>{
         books.push(book);
-        console.log(book.name);
     });
+    return books;
 } catch (error) {
     console.log(error);
 }
@@ -92,17 +90,49 @@ app.post("/addbook",async(req,res)=>{
         const author = req.body.author;
         const result = await db.query("INSERT INTO books (name,description,author,user_id,date_added) VALUES ($1,$2,$3,$4,$5)",[name,description,author,currentUser[0].id,date]);
         if(result.rowCount === 1){
-            books = [];
-            getBooks();
-            console.log("current User: " + currentUser);
-            console.log("Books: " + books);
-            res.render("homepage.ejs",{username: currentUser[0].email,books: books});
+            const book = await getBooks();
+            res.render("homepage.ejs",{username: currentUser[0].email,books: book});
         }else{
             console.log("Failed to add book.")
         }
     } catch (error) {
         console.log(error);
     } 
+});
+
+app.post("/edit",async(req,res)=>{
+   try {
+    console.log(req.body.edit);
+    const result = await db.query("SELECT * FROM books WHERE id = $1",[req.body.edit]);
+    res.render("editbook.ejs",{edit: result.rows[0]});
+   } catch (error) {
+    console.log(error);
+   }
+});
+
+app.post("/editbook",async(req,res)=>{
+try {
+    const name = req.body.name;
+    const description = req.body.description;
+    const author = req.body.author;
+    const id = req.body.id;
+    console.log(name,description,author,id);
+    const result = await db.query("UPDATE books SET name = $1, description = $2, author = $3 WHERE id = $4",[name,description,author,id]);
+    const book = await getBooks();
+    res.render("homepage.ejs",{username: currentUser[0].email,books: book});
+} catch (error) {
+    console.log(error);
+}
+}); 
+
+app.post("/delete",async(req,res)=>{
+try {
+    const result = await db.query("DELETE FROM books WHERE id = $1",[req.body.delete]);
+    const book = await getBooks();
+    res.render("homepage.ejs",{username: currentUser[0].email,books: book});
+} catch (error) {
+    console.log(error);
+}
 });
  
 app.listen(port,()=>{
